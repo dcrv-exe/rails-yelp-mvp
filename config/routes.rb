28c40cdad2d02cdc4b193f2d54c7759bded2873c
Michael McKinstry <michael.mckinstry97@gmail.com>
2022-05-25 17:08:14 +1000

    rails app yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/patients/:id', to: 'patients#show'
+  # get '/app/models/restaurant.rb', to: 'restaurants#index', as: 'index'
+  resources :restaurants, only: %i[index create show new edit] do
+  resources :reviews, only: %i[new create]
+  end
 end
